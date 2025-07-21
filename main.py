379bb6f843de7060d55bd44035d4889157dfb86c
asafwat95 <A.safwat5595@gmail.com>
2025-07-21 16:53:21 -0400

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import requests
 
-# بيانات تطبيقك من Cryptohopper Developer Console
 client_id = '[4KbhRnQjmYMbKADkbsiq9Ux3r2iJnM3sMp5HbNvaKAjHWLnylbnrTHtrsrEBYHvB]'
 client_secret = '[NgsGhPiH7IRNuGGA0NxiH9AxURL1htz6LokCH0jo0pyyCjIdsIyuFjkWASvtnMhW]'
 redirect_uri = 'http://localhost/'
 
-# قناة تيليجرام
-bot_token = '7974713193:AAGaE-sjvB7kTAt_yg6Mp68_xE5lC_czdA8'
-channel_id = '@Cryptoships95'
-
-# الصلاحيات المطلوبة
+# Use the same scope as the one in the Cryptohopper app console
 scope = 'read,notifications,manage,trade'
 
-# رابط التفويض
 authorize_url = 'https://www.cryptohopper.com/oauth2/authorize'
-code_uri = f'{authorize_url}?client_id={client_id}&response_type=code&scope={scope}&state=any&redirect_uri={redirect_uri}'
+code_uri = authorize_url + '?client_id=' + client_id + '&response_type=code&scope=' + scope + '&state=any&redirect_uri=' + redirect_uri
 
-# ✳️ الخطوة دي للمعلومة فقط، متتوقعش إنها تفتح المتصفح تلقائي
-print("افتح الرابط ده في المتصفح وسجل دخولك:")
-print(code_uri)
+# The user logs in, accepts your client authentication request
+requests.get(code_uri)
 
-# ✅ بعد ما تفتح الرابط في المتصفح وتوافق، هيرجعك إلى redirect_uri ومعاه الكود
-# مثال: http://localhost/?code=123456789
-# انسخ الكود وضعه هنا
-code = '123456789'
+# The user will be redirected to the redirect_uri with the code as GET parameter
+# ex: http://localhost/auth?code=123456789
+code = '123456789' # Put the code you got here
+
+token_url = 'https://www.cryptohopper.com/oauth2/token'
+
+#Grab that code and exchange it for an `access_token`
+access_token = requests.post(
+    token_url,
+    data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'redirect_uri': redirect_uri
+    }
+)
+
+print(access_token.json())
+# Response:
+# {
+# 'access_token': '[ACCESS TOKEN]', 
+# 'expires_in': 31556952, 
+# 'token_type': 'Bearer', 
+# 'scope': 'read,notifications,manage,trade', 
+# 'refresh_token': '[REFRESH TOKEN]'
+# }
