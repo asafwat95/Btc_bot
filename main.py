5f43327530ca8e92c4bdc931feb2cda5fdcd0c03
asafwat95 <A.safwat5595@gmail.com>
2025-07-22 18:30:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from bot import run_bot
+from bot import run_bot, send_last_id_telegram
 
 app = Flask(__name__)
 
@@ -10,6 +10,11 @@ def home():
 @app.route('/run')
 def run_once():
     return run_bot()
+
+@app.route('/send_last_id')
+def send_last_id():
+    send_last_id_telegram()
+    return "✅ last_id sent to Telegram!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
