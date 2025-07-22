916d80999a9810b419988f127f796a432d0c0634
asafwat95 <A.safwat5595@gmail.com>
2025-07-22 17:42:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from threading import Thread
-from time import sleep
 from bot import run_bot
 
 app = Flask(__name__)
@@ -13,12 +11,5 @@ def home():
 def run_once():
     return run_bot()
 
-# Looping thread
-def auto_run():
-    while True:
-        run_bot()
-        sleep(60)  # كل دقيقة
-
 if __name__ == '__main__':
-    Thread(target=auto_run, daemon=True).start()
     app.run(host='0.0.0.0', port=10000)
