47d6646ce4ae3dac1674bfd6945c287e38f6ae6a
asafwat95 <A.safwat5595@gmail.com>
2025-07-22 16:06:09 -0400

    App.py

---
+++ App.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from hopper_bot import run_bot
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Hopper bot is online."
+
+@app.route("/run")
+def run():
+    result = run_bot()
+    return f"<pre>{result}</pre>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
