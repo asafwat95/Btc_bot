4f186127b71cb302a9d27df7903d3ddbee564fed
asafwat95 <A.safwat5595@gmail.com>
2025-07-22 17:03:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from app import run_bot
 
-App = Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "✅ Hopper bot is running."
 
-@App.route('/run')
+@app.route('/run')
 def run():
     result = run_bot()
     return f"<pre>{result}</pre>"
 
 if __name__ == '__main__':
-    App.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=10000)
